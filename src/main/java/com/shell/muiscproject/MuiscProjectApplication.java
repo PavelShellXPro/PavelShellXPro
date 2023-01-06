2938a560e2ed0b1fee6df6ba58ecdbe41f362079
Pavel <pavelshelya93@gmail.com>
2023-01-06 21:27:56 +0300

    initial commit

---
+++ src/main/java/com/shell/muiscproject/MuiscProjectApplication.java
@@ -0,0 +1,13 @@
+package com.shell.muiscproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MuiscProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MuiscProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shell/muiscproject/MuiscProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shell.muiscproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MuiscProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
